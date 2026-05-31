ce40133d94e0a8798fc147521597ce934dc63257
Robotwaterfall <jaysonwelshman@gmail.com>
2026-05-31 18:35:09 -0400

    Update game assets and map configurations
    - Replaced the graphics for objects with a new image (objects.png).
    - Modified mainmap.tmx to update object IDs and properties, including the addition of an NPC (Mr. Pigeau) with dialogue.
    - Updated objects.tsx to increase the tile count and added a new tile for the NPC with specific properties.
    - Added new PNG assets for the NPC (pigeau.png) and its idle animations (idle_down_00.png to idle_down_03.png).

--- assets/maps/objects.tsx
+++ assets/maps/objects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="objects" tilewidth="80" tileheight="112" tilecount="8" columns="0">
+<tileset version="1.10" tiledversion="1.12.2" name="objects" tilewidth="80" tileheight="112" tilecount="9" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="1" type="Object">
   <properties>
@@ -98,5 +98,15 @@
    <property name="lifeReg" type="float" value="9999"/>
   </properties>
   <image source="objects/key.png" width="20" height="20"/>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="animation" value="IDLE"/>
+   <property name="animationSpeed" type="float" value="0.1"/>
+   <property name="bodyType" value="StaticBody"/>
+   <property name="life" type="int" value="99999"/>
+   <property name="lifeReg" type="float" value="9999"/>
+  </properties>
+  <image source="objects/pigeau.png" width="38" height="64"/>
  </tile>
 </tileset>
